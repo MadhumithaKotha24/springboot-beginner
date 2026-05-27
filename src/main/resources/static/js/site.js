6da2b038b4f78a2791c901f03f90986ce7aaff8d
Madhu Mitha <madhumithakotha24@gmail.com>
2026-05-27 22:08:12 +0530

    added navbar

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,13 +1,13 @@
 var users = [
 
     {
-        name: "John",
+        name: "John Doe",
         gender: "Male",
         img: "img/john.png"
     },
 
     {
-        name: "Jane",
+        name: "Jane Doe",
         gender: "Female",
         img: "img/jane.png"
     }
@@ -18,13 +18,17 @@ var currentUserIndex = 0;
 
 function toggleCard() {
 
-    currentUserIndex = (currentUserIndex + 1) % users.length;
+    currentUserIndex =
+        (currentUserIndex + 1) % users.length;
 
     var user = users[currentUserIndex];
 
-    document.getElementById("userImage").src = user.img;
+    document.getElementById("userImage").src =
+        user.img;
 
-    document.getElementById("userName").innerHTML = user.name;
+    document.getElementById("userName").innerHTML =
+        user.name;
 
-    document.getElementById("userGender").innerHTML = user.gender;
+    document.getElementById("userGender").innerHTML =
+        user.gender;
 }
